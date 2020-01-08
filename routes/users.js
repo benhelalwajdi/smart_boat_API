63ba1b6c07d7c89c5049577a5bfbb40bd6538094
wajdibenhelal <wajdibenhelal52@live.fr>
2020-01-08 15:45:06 +0100

    get all users from databases

--- routes/users.js
+++ routes/users.js
@@ -4,33 +4,43 @@ var bcrypt = require('bcrypt-nodejs');
 
 var router = express.Router();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({user:'respond with a resource'});
+router.get('/', function (req, res, next) {
+    res.json({user: 'respond with a resource'});
 });
 
-router.get('/login', function (req, res, next) {
-  res.json({user:'login'})
-
+router.get('/Users', function (req, res, next) {
+    const queryString = "SELECT * FROM users";
+    getConnection().query(queryString, req.params.id, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for users: " + err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log("Users fetched successfully");
+        rows.map((row) => {
+            res.json(rows[0]);
+        });
+    });
 });
 
 
 router.post('/register', (req, res) => {
-  let password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  console.log(password);
+    let password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    console.log(password);
 });
 
 var pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'smartboat',
-  port: '8889',
-  connectionLimit: 10
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'smartboat',
+    port: '8889',
+    connectionLimit: 10
 });
 
 
 function getConnection() {
-  return pool;
+    return pool;
 }
 
 module.exports = router;
