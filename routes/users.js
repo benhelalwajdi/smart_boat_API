4a444828cdac82b3bcccad77477dfa84fae5e509
wajdibenhelal <wajdibenhelal52@live.fr>
2020-01-09 17:25:30 +0100

    login method with bcrypt compareSyn

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,33 @@ var bcrypt = require('bcrypt-nodejs');
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.json({user: 'respond with a resource'});
+    res.json({user: 'True'});
+});
+
+
+router.get('/login/:mail/:passwordd', (req, res) => {
+    var bool
+    console.log("Trying to login with EMAIL:" + req.params.mail + " PASSWORD:" + req.params.passwordd);
+    const queryString = "SELECT * FROM users WHERE email = ?";
+    const userMail = req.params.mail;
+    getConnection().query(queryString, [userMail], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for users with email : " + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("User fetched successfully");
+        rows.map((row) => {
+            let password = bcrypt.hashSync(req.body.passwordd, bcrypt.genSaltSync(10));
+            console.log(row.password);
+            console.log(password);
+            if (bcrypt.compareSync(row.password, password)) {
+                res.json(rows[0]);
+            } else {
+                res.json({user: 'login'});
+            }
+        });
+    });
 });
 
 router.get('/Users', function (req, res, next) {
@@ -23,7 +49,6 @@ router.get('/Users', function (req, res, next) {
     });
 });
 
-
 router.post('/register', (req, res) => {
     let password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     console.log(password);
@@ -32,7 +57,7 @@ router.post('/register', (req, res) => {
 var pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'smartboat',
     port: '8889',
     connectionLimit: 10
