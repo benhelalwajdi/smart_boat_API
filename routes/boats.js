a13edbd6d318af0d01205bf9d5b477f895320f32
wajdibenhelal <wajdibenhelal52@live.fr>
2020-01-16 17:34:08 +0100

    response of all boats

--- routes/boats.js
+++ routes/boats.js
@@ -4,10 +4,29 @@ var multer = require('multer');
 
 
 var router = express.Router();
-/* GET users listing. */
+
+
+/* GET boats listing. */
 router.get('/', function (req, res, next) {
     res.json({boats: 'True'});
 });
+
+router.get('/allboats', function (req, res, next) {
+    const queryString = "SELECT * FROM boat";
+    getConnection().query(queryString, req.params.id, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for boats: " + err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log("boats fetched successfully");
+        rows.map((row) => {
+            console.log(rows);
+            res.json(rows);
+        });
+    });
+});
+
 
 var pool = mysql.createPool({
     host: '127.0.0.1',
